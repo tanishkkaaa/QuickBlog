7b442dfcec1189461e82879d1d9d70c94bec6e6b
Tanishka Rajput <175321957+tanishkkaaa@users.noreply.github.com.>
2026-01-19 04:32:15 +0530

    Minor changes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://quickblog.vercel.app"
+    "https://quick-blog-lemon.vercel.app/"
   ],
   credentials: true
 }));
